9cbf95d5dfa6a63c234b9f6911c2c53b24f52c94
PROOHF <mattias.proohf@gmail.com>
2018-03-09 21:46:21 +0100

    init commit

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+function sqlifyTheInputText() {
+    var sqlifyString = document.getElementById('input').value;
+    var sqlStart = "('";
+    var sqlEnd = "')";
+    sqlifyString = sqlifyString.replace(/\n/g, "','");
+    var sqlifyResult = sqlStart.concat(sqlifyString, sqlEnd);
+    document.getElementById("output").value = sqlifyResult;
+ }
+
+function highlightOutput() {
+    var element = document.getElementById("output");
+    element.classList.add("is-valid");
+}
+
+function copyOutputToClipboard() {
+    var copyText = document.getElementById("output");
+    copyText.select();
+    document.execCommand("Copy");
+
+    highlightOutput()
+
+    setTimeout(function () {
+        var element = document.getElementById("output");
+        element.classList.remove("is-valid");
+    }, 250); 
+}
+
+function clearTextareas() {
+    var inputTextarea = document.getElementById("input");
+    var outputTextarea = document.getElementById("output");
+    inputTextarea.value = '';
+    outputTextarea.value = '';
+}
